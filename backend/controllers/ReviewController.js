1ee13ce4207aeb90e533cfe40b4d696516edb3b1
betysr <betysr96@gmail.com>
2023-02-10 10:19:03 +0100

    db conntection and routes done

--- backend/controllers/ReviewController.js
+++ backend/controllers/ReviewController.js
@@ -4,7 +4,18 @@ export const getReviews = async (req, res) => {
     try {
         const firstReview = await Review.findOne({});
         console.log("firstReview",firstReview);
-        res.status(200).json({ success: true, result: firstReview });
+        res.status(200).json({result: firstReview });
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+
+export const addReview = async (req, res) => {
+    try {
+        const review = new Review(req.body);
+        await review.save();
+        res.status(200).json({result: review });
     } catch (error) {
         res.status(500).json({message: error.message});
     }
