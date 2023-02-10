b8ea577ccc7fde36f962f17dde58e69006b4390c
mysr3809 <myasar0938@gmail.com>
2023-02-10 15:42:23 +0100

    country

---
+++ frontend/src/components/homePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CountrySelect from './CountrySelect'
+
+const HomePage = () => {
+  return (
+    <div>
+      <CountrySelect />
+    </div>
+  )
+}
+
+export default HomePage
